a1ed3919e9fafe1baee04c0064ada63d658afbb5
Amir <amir.abdullaev@email.cz>
2022-05-19 22:36:08 +0200

    create datebase and set up it then add some info

--- mysite/cars/models.py
+++ mysite/cars/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=200)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class id_Brend_and_id_Category(models.Model):
+    id_Brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    id_Category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
+    'house.apps.HouseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
