36af634dde3651186e22a4080143cff584412c94
Amir Abdullaev <amir.abdullaev@emai.cz>
2022-05-23 20:43:54 +0200

    edit database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cars',
+        'NAME': 'mysite',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '',
